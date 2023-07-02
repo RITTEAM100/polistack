a28098e187a3c3d16789c274b738b86b9ccf38c1
Rishabh Arora <rishabh.arora@sap.com>
2023-07-01 23:12:38 -0700

    static files

--- polistack/settings.py
+++ polistack/settings.py
@@ -121,8 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
+# Configure static file storage for Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = "static/"
+# Configure static file serving from the Heroku app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
